ac69f02bdab22ecba82320e3a55bc4b69f4900d9
23Yashika <154040835+23Yashika@users.noreply.github.com>
2024-02-07 21:25:14 +0530

    Update Hello.java
    Sop change

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
   public static void main (String[] args){
-    System.out.println("my first java code HELLO WORLD");
+    System.out.println("my t java code HELLO WORLD");
   }
 }
